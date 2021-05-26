c071af0882eac3f5ba134e25dd1b8920570d72bc
raghavven <raghav011986@gmail.com>
2021-05-27 01:02:00 +0530

    changed the function wronly copied as canvas into zen

--- molecool/molecool.py
+++ molecool/molecool.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     # Do something if this file is invoked on its own
     print(canvas())
 
-def canvas(with_attribution=True):
+def zen(with_attribution=True):
     """
     The Zen of Python, by Tim Peters
 
